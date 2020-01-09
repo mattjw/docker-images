f4eae4cd56716afb73c0d26cee3b37c9a343ab13
Matt J Williams <matt-williams@cookpad.com>
2020-01-09 23:54:19 +0000

    Restructure into monorepo of images.

--- circleci-python-java8/publish.sh
+++ circleci-python-java8/publish.sh
@@ -9,9 +9,9 @@ set -u
 # Params
 
 GITHUB_USERNAME="mattjw"
-DOCKER_REPO="circleci"
+GITHUB_REPO="docker-images"
 
-IMAGE_NAME="python-java8"
+IMAGE_NAME="circleci-python-java8"
 IMAGE_TAG="3.6.8"
 
 #
@@ -19,7 +19,7 @@ IMAGE_TAG="3.6.8"
 
 docker login -u ${GITHUB_USERNAME} -p ${GITHUB_TOKEN} docker.pkg.github.com
 
-IMAGE_PATH="docker.pkg.github.com/${GITHUB_USERNAME}/${DOCKER_REPO}/${IMAGE_NAME}:${IMAGE_TAG}"
+IMAGE_PATH="docker.pkg.github.com/${GITHUB_USERNAME}/${GITHUB_REPO}/${IMAGE_NAME}:${IMAGE_TAG}"
 
 echo "Building ${IMAGE_PATH}"
 docker build -t "${IMAGE_PATH}" .
