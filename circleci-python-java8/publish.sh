bbd7adebed4665037cb791c82a2485cbd1987b4a
Matt J Williams <matt-williams@cookpad.com>
2020-01-10 00:04:18 +0000

    Push images to GH packages.

--- circleci-python-java8/publish.sh
+++ circleci-python-java8/publish.sh
@@ -12,7 +12,7 @@ GITHUB_USERNAME="mattjw"
 GITHUB_REPO="docker-images"
 
 IMAGE_NAME="circleci-python-java8"
-IMAGE_TAG="3.6.8"
+IMAGE_TAG="py3.6.8"
 
 #
 # Build and publish
@@ -21,8 +21,8 @@ docker login -u ${GITHUB_USERNAME} -p ${GITHUB_TOKEN} docker.pkg.github.com
 
 IMAGE_PATH="docker.pkg.github.com/${GITHUB_USERNAME}/${GITHUB_REPO}/${IMAGE_NAME}:${IMAGE_TAG}"
 
-echo "Building ${IMAGE_PATH}"
+echo -e "\nBuilding ${IMAGE_PATH}\n"
 docker build -t "${IMAGE_PATH}" .
 
-echo "Pushing ${IMAGE_PATH}"
+echo -e "\nPushing ${IMAGE_PATH}\n"
 docker push ${IMAGE_PATH}
