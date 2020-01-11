cd342fa8988a7f1413f37f064a4dc481dc01ed2f
Matt J Williams <matt-williams@cookpad.com>
2020-01-11 19:46:08 +0000

    Build via get-poetry

--- circleci-python-java8/publish.sh
+++ circleci-python-java8/publish.sh
@@ -25,20 +25,30 @@ docker build -t "${IMAGE_PATH}" .
 #
 # Re-tag
 
+echo -e "\nRe-tagging for Github Packages\n"
+
 TRUNCATED_HASH=$( \
-    docker inspect --format='{{index .RepoDigests 0}}' ${IMAGE_PATH} | \
-    sed 's/.*@sha256:\(.*\)/\1/' | \
-    head -c 6
+    docker inspect --format='{{.Id}}' ${IMAGE_PATH} | \
+    cut -d ':' -f 2 | \
+    head -c 6 \
 )
+if [[ -z "${TRUNCATED_HASH}" ]]; then
+    echo "Failed to obtain truncated hash ${TRUNCATED_HASH}"
+    exit 1
+fi
+
 HASHED_IMAGE_PATH=${IMAGE_PATH}-${TRUNCATED_HASH}
 
-echo -e "\nRe-tagging as ${HASHED_IMAGE_PATH}\n"
+echo -e "Image for Github Packages: ${HASHED_IMAGE_PATH}"
 docker tag ${IMAGE_PATH} ${HASHED_IMAGE_PATH}
 
 #
 # Push
 
+docker login -u ${GITHUB_USERNAME} -p ${GITHUB_TOKEN} docker.pkg.github.com
+
+echo -e "\nPushing ${IMAGE_PATH}\n"
+docker push ${IMAGE_PATH}
+
 echo -e "\nPushing ${HASHED_IMAGE_PATH}\n"
 docker push ${HASHED_IMAGE_PATH}
-
-docker login -u ${GITHUB_USERNAME} -p ${GITHUB_TOKEN} docker.pkg.github.com
